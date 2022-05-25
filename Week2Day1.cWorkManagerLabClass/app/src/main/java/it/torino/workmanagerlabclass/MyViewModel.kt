24ee4bcfc3952463ff5432abb2c68bcf628711e4
Fabio Ciravegna <f.ciravegna@shef.ac.uk>
2022-05-25 13:23:30 +0200

    simplified live data code

--- Week2Day1.cWorkManagerLabClass/app/src/main/java/it/torino/workmanagerlabclass/MyViewModel.kt
+++ Week2Day1.cWorkManagerLabClass/app/src/main/java/it/torino/workmanagerlabclass/MyViewModel.kt
@@ -10,13 +10,11 @@ import kotlinx.coroutines.CoroutineScope
 
 
 class MyViewModel(application: Application) : AndroidViewModel(application) {
-    var TAG = this::class.simpleName
     private val context: Context = application
     // this will track the value of the counter in the Service and report it to the MainActivity
     var currentCounter: MutableLiveData<Int> = CounterService.counter
 
     fun startCounter() {
-        currentCounter = CounterService.counter
         RestarterBroadcastReceiver.startWorker(context)
     }
 
